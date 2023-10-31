8f022a6055f320a37f06e3932bcba574247caba5
HongTorxh <ghdtack@ajou.ac.kr>
2023-10-31 16:31:50 +0900

    security 제거

--- src/main/java/com/example/CoEduServer/config/SecurityConfig.java
+++ src/main/java/com/example/CoEduServer/config/SecurityConfig.java
@@ -1,25 +1,25 @@
-package com.example.CoEduServer.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-
-@RequiredArgsConstructor
-
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(
-                        authorize -> authorize
-                                .requestMatchers("/", "/**").permitAll()
-                );
-        return http.build();
-    }
-}
+//package com.example.CoEduServer.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@RequiredArgsConstructor
+//
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+//        http
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .sessionManagement(AbstractHttpConfigurer::disable)
+//                .formLogin(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(
+//                        authorize -> authorize
+//                                .requestMatchers("/", "/**").permitAll()
+//                );
+//        return http.build();
+//    }
+//}
