286a84632762ccf46f72aab7db67c371300b65a4
HongTorxh <ghdtack@ajou.ac.kr>
2023-11-22 20:10:06 +0900

    File DTO

--- src/main/java/com/example/CoEduServer/dto/res/GetFilesDTO.java
+++ src/main/java/com/example/CoEduServer/dto/res/GetFilesDTO.java
@@ -2,6 +2,7 @@ package com.example.CoEduServer.dto.res;
 
 import com.example.CoEduServer.domain.BaseTimeEntity;
 import com.example.CoEduServer.domain.User_File;
+import com.example.CoEduServer.domain.enums.Role;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -11,7 +12,9 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class GetFilesDTO extends BaseTimeEntity {
+    private Long userId;
     private Long fileId;
+    private Role role;
     private String fileName;
     private String language;
     private String fileDetail;
@@ -22,10 +25,12 @@ public class GetFilesDTO extends BaseTimeEntity {
     public static GetFilesDTO toEntity(User_File user_file) {
         return GetFilesDTO.builder()
                 .fileId(user_file.getFile().getId())
+                .userId(user_file.getUser().getId())
                 .fileName(user_file.getFile().getFileName())
                 .language(user_file.getFile().getLanguage())
                 .fileDetail(user_file.getFile().getFileDetail())
                 .fileHash(user_file.getFile().getFileHash())
+                .role(user_file.getFile().getRole())
                 .createdTime(user_file.getFile().getCreatedTime())
                 .modifiedTime(user_file.getFile().getModifiedTime())
                 .build();
