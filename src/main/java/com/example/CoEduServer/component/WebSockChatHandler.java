282c6adbdda9729db7ec808397e391e9f2e61c2e
HongTorxh <ghdtack@ajou.ac.kr>
2023-11-30 23:34:53 +0900

    Chatting 끊김 현상 발생

--- src/main/java/com/example/CoEduServer/component/WebSockChatHandler.java
+++ src/main/java/com/example/CoEduServer/component/WebSockChatHandler.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -27,5 +28,16 @@ public class WebSockChatHandler extends TextWebSocketHandler {
         ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
         ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
         room.handleActions(session, chatMessage, chatService);
+    }
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception{
+
+        log.info("chatWebsocketHandler afterConnectionEstablished:" + session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+
+        log.info("chatWebsocketHandler afterConnectionClosed:" + session + ":" + status);
     }
 }
